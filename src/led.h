3f7b3cf74f78077bda8fefa74ea0bc490f5755d7
Samuel <samistheguns@gmail.com>
2025-05-06 13:11:24 +0700

    code to light up led with width and variable starting point done

---
+++ src/led.h
@@ -0,0 +1,16 @@
+#ifndef LED_H
+#define LED_H
+
+#include <stdint.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void initLED();
+inline void sendNops(uint8_t n);
+void sendBit(uint8_t bit);
+void sendByte(uint8_t byte);
+void sendColor(uint8_t red, uint8_t green, uint8_t blue);
+void sendLeds(uint8_t r, uint8_t g, uint8_t b, int count);
+void lightLEDs(int color, int width, int start);
+
+#endif
